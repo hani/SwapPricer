ae4dadffb2f3cb73a328416ef6dbdd8b2a543f1c
Hani Suleiman <hani@formicary.net>
2011-12-19 17:11:48 -0500

    Need to use LIBOR, but why?!

--- src/main/java/net/formicary/pricer/impl/RateManagerImpl.java
+++ src/main/java/net/formicary/pricer/impl/RateManagerImpl.java
@@ -1,13 +1,13 @@
 package net.formicary.pricer.impl;
 
-import javax.inject.Inject;
-
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.query.Query;
 import net.formicary.pricer.RateManager;
 import net.formicary.pricer.model.Index;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
+
+import javax.inject.Inject;
 
 /**
  * @author hani
@@ -24,16 +24,17 @@ public class RateManagerImpl implements RateManager {
     query.field("tenorUnit").equal(tenor.substring(0, tenor.length() - 1));
     query.field("tenorPeriod").equal(tenor.substring(tenor.length() - 1));
     query.field("fixingDate").equal(date);
-    Index i = query.get();
-    if(i == null) {
+    query.field("name").equal("LIBOR");
+    Index index = query.get();
+    if(index == null) {
       throw new IllegalArgumentException("No rate found for " + currency + " " + tenor + " on " + date);
     }
-    return i.getRate();
+    return index.getRate();
   }
 
   @Override
   public double getDiscountFactor(String currency, String tenorPeriod, LocalDate date, LocalDate valuationDate) {
-    double zero = getZeroRate(currency, tenorPeriod, date);
+    double zero = getZeroRate(currency, tenorPeriod, date) / 100;
     double days = Days.daysBetween(date, valuationDate).getDays();
     return Math.exp(zero * -(days) / 365d);
   }
