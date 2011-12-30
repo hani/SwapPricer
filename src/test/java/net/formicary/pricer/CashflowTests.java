9db0da172ffca22ed7b43be18482b328e436074d
Hani Suleiman <hani@formicary.net>
2011-12-29 21:28:41 -0500

    More test cases

--- src/test/java/net/formicary/pricer/CashflowTests.java
+++ src/test/java/net/formicary/pricer/CashflowTests.java
@@ -75,7 +75,7 @@ public class CashflowTests {
   @DataProvider(name = "singletrade")
   public Object[][] singleTrade() {
     Object[][] data = new Object[1][];
-    data[0] = new Object[]{"LCH00000513426"};
+    data[0] = new Object[]{"LCH00001018118"};
     return data;
   }
 }
