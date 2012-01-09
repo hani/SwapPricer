d48a55cca1feb80a2a5d6899f0975c397b53fcbf
Hani Suleiman <hani@formicary.net>
2012-01-09 09:41:50 -0500

    Reordered imports

--- src/test/java/net/formicary/pricer/CashflowTests.java
+++ src/test/java/net/formicary/pricer/CashflowTests.java
@@ -1,9 +1,5 @@
 package net.formicary.pricer;
 
-import java.io.*;
-import java.util.*;
-import javax.xml.bind.JAXBException;
-
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import net.formicary.pricer.model.Cashflow;
@@ -16,6 +12,16 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import javax.xml.bind.JAXBException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -38,7 +44,7 @@ public class CashflowTests {
     generator = injector.getInstance(CashflowGenerator.class);
   }
 
-  @Test(dataProvider = "singletrade")
+  @Test(dataProvider = "trades")
   public void generateCashflows(String id) throws Exception {
     List<Cashflow> actualFlows;
     try {
