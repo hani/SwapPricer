650d3967ac4f8d019afc5d4ea3c27f916462e17f
Hani Suleiman <hani@formicary.net>
2012-01-18 10:24:21 -0500

    Support rate dirs

--- src/main/java/net/formicary/pricer/loader/RateLoader.java
+++ src/main/java/net/formicary/pricer/loader/RateLoader.java
@@ -1,8 +1,9 @@
 package net.formicary.pricer.loader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -22,33 +23,61 @@ public abstract class RateLoader {
   private static final Logger log = LoggerFactory.getLogger(RateLoader.class);
 
   public void importHistoricRates(String file) throws IOException {
+    List<File> files = new ArrayList<File>();
+    File root = new File(file);
+    if(!root.isDirectory()) {
+      files.add(root);
+    } else {
+      files = list(root);
+    }
     long now = System.currentTimeMillis();
-    BufferedReader is = new BufferedReader(new FileReader(file));
-    is.readLine();
-    String line;
     int count = 0;
-    while((line = is.readLine()) != null) {
-      String[] items = line.split("\t");
-      if(items.length < 7) continue;
-      Index index = new Index();
-      index.setCurrency(items[0]);
-      index.setName(items[1]);
-      index.setTenor(items[2] + items[3]);
-      FastDate fixingDate = new FastDate(Integer.parseInt(items[4].substring(6, 10)), Integer.parseInt(items[4].substring(3, 5)), Integer.parseInt(items[4].substring(0, 2)));
-      index.setFixingDate(fixingDate);
-      index.setEffectiveDate(new FastDate(Integer.parseInt(items[5].substring(6, 10)), Integer.parseInt(items[5].substring(3, 5)), Integer.parseInt(items[5].substring(0, 2))));
-      try {
-        index.setRate(TypeFormat.parseDouble(items[6]));
-        index.setRegulatoryBody(items[7]);
-        save(index);
-        if(++count % 20000 == 0) {
-          log.info("Saved " + count  + " rates");
+    for(File f : files) {
+      log.info("Parsing " + f);
+      BufferedReader is = new BufferedReader(new FileReader(f));
+      is.readLine();
+      String line;
+      while((line = is.readLine()) != null) {
+        String[] items = line.split("\t");
+        if(items.length < 7) continue;
+        Index index = new Index();
+        index.setCurrency(items[0]);
+        index.setName(items[1]);
+        index.setTenor(items[2] + items[3]);
+        FastDate fixingDate = new FastDate(Integer.parseInt(items[4].substring(6, 10)), Integer.parseInt(items[4].substring(3, 5)), Integer.parseInt(items[4].substring(0, 2)));
+        index.setFixingDate(fixingDate);
+        index.setEffectiveDate(new FastDate(Integer.parseInt(items[5].substring(6, 10)), Integer.parseInt(items[5].substring(3, 5)), Integer.parseInt(items[5].substring(0, 2))));
+        try {
+          index.setRate(TypeFormat.parseDouble(items[6]));
+          index.setRegulatoryBody(items[7]);
+          save(index);
+          if(++count % 20000 == 0) {
+            log.info("Saved " + count  + " rates");
+          }
+        } catch(NumberFormatException e) {
+          log.info("Invalid rate found for index " + index + ":" + items[6]);
         }
-      } catch(NumberFormatException e) {
-        log.info("Invalid rate found for index " + index + ":" + items[6]);
       }
+      is.close();
     }
     log.info("Initialised historic rates in " + (System.currentTimeMillis() - now) + "ms with " + count + " rates");
+  }
+
+  private List<File> list(final File root) {
+    final List<File> files = new ArrayList<File>();
+    files.addAll(Arrays.asList(root.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        if(file.getName().contains(("00003"))) {
+          return true;
+        }
+        if(file.isDirectory()) {
+          files.addAll(list(file));
+        }
+        return false;
+      }
+    })));
+    return files;
   }
 
   protected abstract void save(Index index);
